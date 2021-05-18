49b05be4226d03099736d5f589021030fde7da2c
devide <devide@126.com>
2021-05-18 17:33:55 +0800

    1

--- src/api/role/index.js
+++ src/api/role/index.js
@@ -21,6 +21,27 @@ export default
                 method: "post",
                 data
             })
+        },
+        edit_role:(data)=>{
+            return request({
+                url: "/role/edit",
+                method: "post",
+                data
+            })
+        },
+        find_role:(data)=>{
+            return request({
+                url: "/role/find",
+                method: "post",
+                data
+            })
+        },
+        role_menus:(data)=>{
+            return request({
+                url: "/role/menulist",
+                method: "get",
+                params:data
+            })
         }
 
     }
--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -7,7 +7,8 @@
           icon="el-icon-plus"
           size="small"
           @click="add_role"
-        >新增角色</el-button>
+          >新增角色</el-button
+        >
       </template>
     </base-query>
     <el-table :data="list">
@@ -33,10 +34,9 @@
               <i class="el-icon-setting" style="font-size: 16px" />
             </span>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item @click.native="role_edit(scope.row)">编辑</el-dropdown-item>
-            </el-dropdown-menu>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item @click.native="role_menu(scope.row)">关联菜单</el-dropdown-item>
+              <el-dropdown-item @click.native="role_edit(scope.row)"
+                >编辑</el-dropdown-item
+              >
             </el-dropdown-menu>
           </el-dropdown>
         </template>
@@ -53,6 +53,7 @@
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
     />
+    <!--角色添加窗口-->
     <el-dialog :title="dialogtitle" :visible.sync="dialogVisible" top="10px">
       <el-form
         ref="role_form"
@@ -83,6 +84,40 @@
         <el-button type="primary" @click="save_role_handle">确定</el-button>
       </div>
     </el-dialog>
+    <!--角色编辑窗口-->
+    <el-dialog
+      :title="dialogtitle"
+      :visible.sync="dialogVisible"
+      @opened="roleform_openhandle"
+    >
+      <el-form
+        :model="role_form_edit"
+        label-width="80px"
+        label-position="right"
+        size="small"
+      >
+        <el-form-item label="名称">
+          <el-input v-model="role_form_edit.title"></el-input>
+        </el-form-item>
+        <el-form-item label="功能权限">
+          <el-tree
+            ref="permis_tree_edit"
+            :data="permissiontree"
+            :props="treeconfig"
+            show-checkbox
+            check-strictly
+            accordion
+            node-key="id"
+          />
+        </el-form-item>
+      </el-form>
+      <div slot="footer">
+        <el-button type="danger" @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submit_role_form_edit"
+          >确定</el-button
+        >
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -111,6 +146,7 @@ export default {
         adduser: store.getters.userinfo.id,
         status: 1,
       },
+      role_form_edit: {},
       treeconfig: {
         children: "children",
         label: "title",
@@ -157,7 +193,19 @@ export default {
       this.dialogtitle = "新增角色";
       this.dialogVisible = true;
     },
-    role_menu(row) {},
+    role_edit(row) {
+      this.dialogtitle = "编辑角色";
+      RoleFn.role_menus({ id: row.id }).then((res) => {
+        this.role_form_edit.id = row.id;
+        this.role_form_edit.code = row.code;
+        this.role_form_edit.title = row.title;
+        this.role_form_edit.status = row.status;
+        this.role_form_edit.menu_nodes = res.menus;
+        this.role_form_edit.status = 1;
+        this.role_form_edit.updateuser = store.getters.userinfo.id
+        this.dialogVisible = true;
+      });
+    },
     handleSizeChange(val) {
       this.pagesize = val;
     },
@@ -172,19 +220,42 @@ export default {
           RoleFn.add_role(this.role_form).then((res) => {
             this.$message(res.msg);
             if (res.code === 1) {
-              this.getlist()
+              this.getlist();
               this.dialogVisible = false;
-              this.$nextTick(() => {
-                this.role_menu.menu_nodes = [];
-                this.$refs.role_form.resetFields()
-                this.$refs.permission_tree.setCheckedKeys([]);
-              });
+              this.role_menu.menu_nodes = [];
+              this.$refs.role_form.resetFields();
+              this.$refs.permission_tree.setCheckedKeys([]);
             }
           });
         }
       });
     },
     node_click_handle() {},
+    submit_role_form_edit() {
+      let cknodes = this.$refs.permis_tree_edit.getCheckedNodes(false, true);
+      this.role_form_edit.menu_nodes = cknodes;
+      RoleFn.edit_role(this.role_form_edit).then(res=>{
+        this.$message.success(res.msg)
+        if(res.code === 1)
+        {
+          this.dialogVisible = false
+        }
+      })
+      
+    },
+    roleform_openhandle() {
+      let nodes = [];
+      this.role_form_edit.menu_nodes.forEach(i=>{
+        console.log(i)
+        nodes.push(i)
+        if(i.menu_permission){
+          i.menu_permission.funs
+          i.menu_permission.editfields
+          i.menu_permission.hidefields
+        }
+      });
+      this.$refs.permis_tree_edit.setCheckedNodes(nodes);
+    },
   },
 };
 </script>
